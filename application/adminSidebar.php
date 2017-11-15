1cf61d366591c3d6b2ccd9e89d09dade1530a4e7
Ben <jlee286@uncc.edu>
2017-11-14 20:06:31 -0500

    Add adminSidebar

---
+++ application/adminSidebar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjaminlee
+ * Date: 11/14/17
+ * Time: 7:57 PM
+ */?>
+
+
+<!DOCTYPE html>
+<html>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+            <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home <span class="sr-only">(Current)</span></a>
+                    <a class="nav-link" href="#">Employees</a>
+                    <a class="nav-link" href="#">Fleet</a>
+                    <a class="nav-link" href="#">Routes</a>
+                    <a class="nav-link" href="#">Schedule</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</html>
