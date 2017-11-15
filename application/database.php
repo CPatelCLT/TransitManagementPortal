cf5805088d107346f5bdb834ecb8a16bced7aaed
Chirag <cpatel36@uncc.edu>
2017-11-15 01:30:07 -0500

    Added DB connection file

---
+++ application/database.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("dbcreds.php");
+
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include('database_error.php');
+    exit();
+}
+?>
