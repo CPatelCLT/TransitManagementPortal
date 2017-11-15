7622c7e5506dadae33b25cfa9745958a5eadf6b4
tray24 <Andrew123>
2017-11-14 21:29:26 -0500

    add admin_employees for real

---
+++ application/admin/admin_employee.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../GroupProjectUI/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../GroupProjectUI/css/dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="../dashboard.html">Dashboard</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active"> <a class="nav-link" href="#"> item1 </a></li>
+                <li class="nav-item"> <a class="nav-link" href="#"> item2 </a></li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</header>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <?php include("views\adminSidebar.php"); ?>
+        <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
+            <?php include("views\AdminEmployeesTable.php"); ?>
+
+        </main>
+    </div>
+</div>
+
+</body>
+<footer class="footer">
+    <div class="container">
+        <form method="post">
+            <button type="submit" style="width: 100%; height: 80px;">Add Employee</button>
+        </form>
+    </div>
+</footer>
+</html>
