1f2e8ab96b05c75cda4285cc0c0adf86e75e5889
Chirag <cpatel36@uncc.edu>
2017-11-17 15:43:58 -0500

    added some more data files

---
+++ application/data/fleet.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chirag
+ * Date: 11/17/2017
+ * Time: 2:33 PM
+ */
+
+require_once('../database.php');
+
+function getAllBuses()
+{
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM fleet');
+    $stmt->execute();
+    $buses = $stmt->fetchAll();
+    $stmt->closeCursor();
+    return $buses;
+}
+
+function getBusesByActive($active) {
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM fleet where active = :active');
+    $stmt->bindParam(':active', $active);
+    $stmt->execute();
+    $buses = $stmt->fetchAll();
+    $stmt->closeCursor();
+    return $buses;
+}
+
+function getBusByID($busID) {
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM fleet where busID = :busID');
+    $stmt->bindParam(':busID', $busID);
+    $stmt->execute();
+    $bus = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $bus;
+}
---
+++ application/data/route.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chirag
+ * Date: 11/17/2017
+ * Time: 2:39 PM
+ */
+
+require_once('../database.php');
+
+function getAllRoutes()
+{
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM routes');
+    $stmt->execute();
+    $routes = $stmt->fetchAll();
+    $stmt->closeCursor();
+    return $routes;
+}
+
+function getRouteByID($routeID) {
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM routes where routeID = :routeID');
+    $stmt->bindParam(':routeID', $routeID);
+    $stmt->execute();
+    $route = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $route;
+}
+
+function getRouteSequence($routeID) {
+    global $db;
+    $stmt = $db->prepare('select routesequence.*, stops.* from routesequence inner join stops on routesequence.stopID = stops.stopID where routeID = :routeID;');
+    $stmt->bindParam(':routeID', $routeID);
+    $stmt->execute();
+    $routeSequence = $stmt->fetchAll();
+    $stmt->closeCursor();
+    return $routeSequence;
+}
