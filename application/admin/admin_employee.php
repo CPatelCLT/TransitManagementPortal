f706550652cc2fdc7d403ea8cd187a9dfc998b0f
Chirag <cpatel36@uncc.edu>
2017-12-02 18:49:51 -0500

    Removed integrity check for external js references

--- application/admin/admin_employee.php
+++ application/admin/admin_employee.php
@@ -176,10 +176,7 @@ function showEmployee($empID)
 </div>
 </div>
 </div>
-<script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
 <script>
@@ -361,12 +358,8 @@ function showEmployee($empID)
         </main>
     </div>
 </div>
-<script
-        src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
-        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- application/customer/customer_home.php
+++ application/customer/customer_home.php
@@ -26,9 +26,8 @@
         <img width="75%" src="https://img00.deviantart.net/ec24/i/2013/363/0/1/totoro_bus_stop__art_trade_practice__p_by_blueroses4-d6zwpk6.jpg" class="d-block mx-auto img-responsive">
     </main>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
